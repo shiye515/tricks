89a2b7e5bc1dc7461314d689f0657a558390be50
shiye515 <shiye515@gmail.com>
2014-03-19 16:31:48 +0800

    Update tricks.js
    add 数字补0

--- tricks.js
+++ tricks.js
@@ -1,5 +1,17 @@
 function htmlEncode(str) {
-var div = document.createElement("div");
-div.appendChild(document.createTextNode(str));
-return div.innerHTML;
+    var div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+}
+
+
+//数字补0
+function add0(str, length) {
+    str += '';
+    return new Array(length - str.length + 1).join("0") + str;
+}
+
+function dateToStr(date) {
+    var d = new Date(date);
+    return [d.getFullYear(), add0(d.getMonth() + 1, 2), add0(d.getDate(), 2)].join('');
 }
