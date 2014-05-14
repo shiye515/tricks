10d5ac61da5167027cdc4f45327bc3dec37680ab
shiye515 <shiye515@gmail.com>
2014-05-14 20:56:59 +0800

    Update tricks.js
    add对象深复制

--- tricks.js
+++ tricks.js
@@ -3,7 +3,11 @@ function htmlEncode(str) {
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
 }
-
+function htmlDecode(s){  
+    var div = document.createElement('div');  
+    div.innerHTML = s;  
+    return div.innerText || div.textContent;  
+}
 
 //数字补0
 function add0(str, length) {
@@ -15,3 +19,6 @@ function dateToStr(date) {
     var d = new Date(date);
     return [d.getFullYear(), add0(d.getMonth() + 1, 2), add0(d.getDate(), 2)].join('');
 }
+
+//对象深复制
+var cloneObj = JSON.parse(JSON.stringify(obj));
