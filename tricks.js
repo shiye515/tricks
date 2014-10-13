06a410485e20375313574cef637ddafbf3a69495
shiye515 <shiye515@gmail.com>
2014-10-13 18:02:50 +0800

    添加 解析url 方法 数字取整技巧
    添加 解析url 方法 数字取整技巧

--- tricks.js
+++ tricks.js
@@ -1,15 +1,21 @@
+//htmlEncode
+
 function htmlEncode(str) {
     var div = document.createElement("div");
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
 }
-function htmlDecode(s){  
-    var div = document.createElement('div');  
-    div.innerHTML = s;  
-    return div.innerText || div.textContent;  
+
+//htmlDecode
+
+function htmlDecode(s) {
+    var div = document.createElement('div');
+    div.innerHTML = s;
+    return div.innerText || div.textContent;
 }
 
 //数字补0
+
 function add0(str, length) {
     str += '';
     return new Array(length - str.length + 1).join("0") + str;
@@ -21,6 +27,7 @@ function dateToStr(date) {
 }
 
 //对象深复制
+
 var cloneObj = JSON.parse(JSON.stringify(obj));
 
 //编码转换（to Unicode）
@@ -33,3 +40,42 @@ function toUnicode(string) {
     }
     return str;
 }
+
+//解析url
+
+function parseURL(url) {
+    var a = document.createElement('a');
+    a.href = url;
+    return {
+        source: url,
+        protocol: a.protocol.replace(':', ''),
+        host: a.hostname,
+        port: a.port,
+        query: a.search,
+        params: (function () {
+            var ret = {},
+                seg = a.search.replace(/^\?/, '').split('&'),
+                len = seg.length,
+                i = 0,
+                s;
+            for (; i < len; i++) {
+                if (!seg[i]) {
+                    continue;
+                }
+                s = seg[i].split('=');
+                ret[s[0]] = s[1];
+            }
+            return ret;
+        })(),
+        file: (a.pathname.match(/\/([^\/?#]+)$/i) || [, ''])[1],
+        hash: a.hash.replace('#', ''),
+        path: a.pathname.replace(/^([^\/])/, '/$1'),
+        relative: (a.href.match(/tps?:\/\/[^\/]+(.+)/) || [, ''])[1],
+        segments: a.pathname.replace(/^\//, '').split('/')
+    };
+}
+
+//数字取整 |0 或者 ~~
+
+(12.4 / 4.13) | 0; //结果为3
+~~(12.4 / 4.13); //结果为3
